f48e168782772a0d6ea064a4a0a64d24f6864a54
Snowsita <Snowsita@users.noreply.github.com>
2025-06-29 18:59:37 -0600

    feat: completed the website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Home} from './pages/home/home';
+import {Chat} from './pages/chat/chat';
+import {Journal} from './pages/journal/journal';
+import {Login} from './pages/login/login';
+import {Recommendations} from './pages/recommendations/recommendations';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: 'chat',
+    component: Chat
+  },
+  {
+    path: 'journal',
+    component: Journal
+  },
+  {
+    path: '',
+    component: Login
+  },
+  {
+    path: 'recommendations',
+    component: Recommendations
+  },
+];
